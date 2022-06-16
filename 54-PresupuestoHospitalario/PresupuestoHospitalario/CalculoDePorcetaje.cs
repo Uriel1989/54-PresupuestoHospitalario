f8d6f8d1c6a81d43f7082c19f4f95aee984e8f87
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:07:28 -0300

    Add files via upload

---
+++ 54-PresupuestoHospitalario/PresupuestoHospitalario/CalculoDePorcetaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PresupuestoHospitalario
+{
+    class CalculoDePorcetaje
+    {
+        public int Ginecologia (int prespusto)
+        {
+            return (prespusto * 40) / 100;
+        }
+
+        public int Traumatologia (int prespuesto)
+        {
+            return (prespuesto * 30) / 100;
+        }
+
+        public int Pediatria (int presupuesto)
+        {
+            return (presupuesto * 30) / 100;
+        }
+    }
+}
---
+++ 54-PresupuestoHospitalario/PresupuestoHospitalario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PresupuestoHospitalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*En un hospital existen tres áreas: Ginecología, Pediatría, Traumatologia, el presupuesto anual del hospital se reparte conforme a la sig. tabla:
+             *Área            Porcentaje del presupuesto
+             *Ginecología           40%
+             *Traumatologia         30%
+             *Pediatría             30%
+             *Obtener la cantidad de dinero que recibirá cada área, para cualquier monto presupuestal.*/
+            CalculoDePorcetaje PrespuestoDeSector = new CalculoDePorcetaje();
+
+            Console.WriteLine("Ingrese el monto presupuestario total del hospital:");
+            int presupuesto = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El monto que recibira Ginecologia es de: " + PrespuestoDeSector.Ginecologia(presupuesto));
+
+            Console.WriteLine("El monto que recibira el sector de Traumatologia es de: " + PrespuestoDeSector.Traumatologia(presupuesto));
+
+            Console.WriteLine("El monto que recibira el sector de Pediatria es de: " + PrespuestoDeSector.Pediatria(presupuesto));
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
